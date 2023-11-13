f5fda61175ccfebc62de63553e973196f4c3da7d
Zain <84030976+Zainshah78@users.noreply.github.com>
2023-11-13 22:03:01 +0500

    Mega project of complex blog

---
+++ 12MegaBlog/src/pages/AllPosts.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Container, PostCard } from "../components";
+import appwriteService from "../appwrite/config";
+
+function AllPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {}, []);
+  appwriteService.getPosts([]).then((posts) => {
+    if (posts) {
+      setPosts(posts.documents);
+    }
+  });
+  return (
+    <div className="w-full">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default AllPosts;
